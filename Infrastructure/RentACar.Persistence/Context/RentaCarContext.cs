3a6af2d8eefe396c1ca2bf59110a0676708e0591
Beyazid21 <beyazimustafayev55@gmail.com>
2024-10-11 00:43:42 +0400

    Database ilə əlaqə üçün lazım olanlar edildi

---
+++ Infrastructure/RentACar.Persistence/Context/RentaCarContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentaCar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Persistence.Context
+{
+    public class RentaCarContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=E-CommerceApi;Trusted_Connection=True;TrustServerCertificate=true");
+
+
+        }
+        public DbSet<About> Abouts { get; set; } 
+        public DbSet<Banner> Banners { get; set; } 
+        public DbSet<Brand> Brands { get; set; } 
+        public DbSet<Car> Cars { get; set; } 
+        public DbSet<CarDescription> CarDescriptions { get; set; } 
+        public DbSet<CarFeature> CarFeature { get; set; } 
+        public DbSet<CarPricing> CarPricings { get; set; } 
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Contact> Contacts { get; set; } 
+        public DbSet<Feature> Features { get; set; } 
+        public DbSet<FooterAddress> FooterAddresses { get; set; } 
+        public DbSet<Location> GetLocations { get; set; } 
+        public DbSet<Pricing> Pricings { get; set; } 
+        public DbSet<Service> Services { get; set; } 
+        public DbSet<SocialMedia> SocialMedias { get; set; } 
+        public DbSet<Testimonial> Testimonials { get; set; } 
+
+
+
+    }
+}
